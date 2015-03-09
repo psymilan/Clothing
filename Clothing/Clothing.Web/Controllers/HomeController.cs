2caa1701d94b1e0b296800e489fe168d926795eb
Milan Manasievski <milan.manasievski@strv.com>
2015-03-09 21:17:28 +0100

    contact page fixed

--- Clothing/Clothing.Web/Controllers/HomeController.cs
+++ Clothing/Clothing.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 using Clothing.Web.Data;
 using System.Linq;
 
@@ -6,7 +9,9 @@ namespace Clothing.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public int UserId { get { return int.Parse(User.Identity.Name); }
+        public int UserId
+        {
+            get { return int.Parse(User.Identity.Name); }
         }
         private readonly IClothingRepository repository;
         public HomeController(IClothingRepository repo)
@@ -44,5 +49,41 @@ namespace Clothing.Web.Controllers
             }
             return View();
         }
+
+        public ActionResult SendEmail(ContactForm contactForm)
+        {
+            var mail = new MailMessage();
+            mail.To.Add("shirin.vakhidova@gmail.com");
+            mail.From = new MailAddress(contactForm.Email, contactForm.Name, System.Text.Encoding.UTF8);
+            mail.Subject = contactForm.Name + " - " + contactForm.Email + " wrote you on SHIRIN-VAHIDI.COM";
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            var body = "Email: " + contactForm.Email + "<br />" + contactForm.Message;
+            mail.Body = body;
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+            mail.ReplyTo = new MailAddress(contactForm.Email);
+            var client = new SmtpClient
+            {
+                Credentials = new NetworkCredential("psymilan99@gmail.com", "milan1989"),
+                Port = 587,
+                Host = "smtp.gmail.com",
+                EnableSsl = true
+            };
+
+            client.Send(mail);
+
+            TempData["email"] = "Message successfully sent. We will reply shortly!";
+
+            return RedirectToAction("Contact");
+
+        }
+    }
+
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
     }
 }
